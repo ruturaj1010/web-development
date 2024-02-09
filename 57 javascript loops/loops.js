28ebd44f783edf798b9c60c0ebddc9a97198406e
Ruturaj <ruturajnikam1010@gmail.com>
2024-02-09 22:46:34 +0530

    commit push 	new file:   57 javascript loops/loops.js 	modified:   time.txt

---
+++ 57 javascript loops/loops.js
@@ -0,0 +1,37 @@
+console.log("javascript tutorial no 57 of loops")
+
+// a = 1;
+
+// for (let i = 0; i < 50 ; i++) {
+//     console.log(a + i)
+// }
+
+
+// let obj = {
+//     "name" : "Ruturaj",
+//     "working": "No",
+//     "company" : "student"
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call (obj, key)) {
+//         const element = obj[key];
+//         console.log(key , element)
+//     }
+// }
+
+// for(const c of "Ruturaj") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+let j = 10;
+do {
+    console.log(j);
+    j++;
+} while (j < 5)
