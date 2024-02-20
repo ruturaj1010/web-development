cf7a68e73347f5027b458f4c46fd356e06204efb
Ruturaj <ruturajnikam1010@gmail.com>
2024-02-20 18:05:25 +0530

    commit push 	modified:   71 Inserting and removing elements using js/insert_Remove.js 	modified:   71 Inserting and removing elements using js/insert_remove.html

--- 71 Inserting and removing elements using js/insert_Remove.js
+++ 71 Inserting and removing elements using js/insert_Remove.js
@@ -1,1 +1,22 @@
 console.log(document.querySelector(".box").innerHTML)
+
+// document.querySelector(".container").innerHTML
+// document.querySelector(".container").innerText
+// document.querySelector(".container").outerHTML
+// document.querySelector(".container").tagName
+
+// document.querySelector(".box").hasAttribute("style")
+// document.querySelector(".box").getAttribute("style")
+// document.querySelector(".box").setAttribute("style","display:inline")
+
+// let div = document.createElement("div")
+// div.innerHTML = "I have been inserted in body <b>by Ruturaj</b>"
+// div.setAttribute("class", "created")
+// document.querySelector(".container").append(div)
+
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("afterbegin" , "<h1>I am inserted using adjacentHTML</h1>")
+
+
+/* 16:41*/
